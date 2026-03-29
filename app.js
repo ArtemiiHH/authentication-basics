89ffb19b3408ec8f2e792ba456065176815d5c1c
ArtemiiHH <artemiibarashkov2003@gmail.com>
2026-03-29 17:58:04 +0300

    Setup LocalStrategy func

--- app.js
+++ app.js
@@ -36,6 +36,36 @@ app.post("/sign-up", async (req, res, next) => {
   }
 });
 
+// Passport strategies
+
+// Setup LocalStrategy
+passport.use(
+  new LocalStrategy(async (username, password, done) => {
+    try {
+      const { rows } = await pool.query(
+        "SELECT * FROM users WHERE username = $1",
+        [username],
+      );
+      const user = rows[0];
+
+      if (!user) {
+        return done(null, false, {
+          message: "Incorrect username",
+        });
+      }
+      if (user.password !== password) {
+        return done(null, false, {
+          message: "Incorrect password",
+        });
+      }
+
+      return done(null, user);
+    } catch (err) {
+      return done(err);
+    }
+  }),
+);
+
 app.listen(3000, (error) => {
   if (error) {
     throw error;
