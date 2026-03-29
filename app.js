354ce16ccd49bd4231f0a35b241c16c2296f3af6
ArtemiiHH <artemiibarashkov2003@gmail.com>
2026-03-29 16:27:01 +0300

    Add paths in app

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const { Pool } = require("pg");
 const express = require("express");
 const session = require("express-session");
 const passport = require("passport");
+const { error } = require("node:console");
 const LocalStrategy = require("passport-local").Strategy;
 
 const pool = new Pool({
@@ -12,4 +13,19 @@ const pool = new Pool({
 });
 
 const app = express();
-app.set();
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(session({ secret: "cats", resave: false, saveUninitialized: false }));
+app.use(passport.session());
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => res.render("index"));
+
+app.listen(3000, (error) => {
+  if (error) {
+    throw error;
+  }
+
+  console.log("app listening on port 3000!");
+});
